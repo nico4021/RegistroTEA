b2e727d4d20af9e7a711c230be874f8514fe91b3
Luciano Castillo <luciano@localhost.localdomain>
2015-09-01 11:52:03 -0300

    Añadiendo informacion en settings

--- ProyectoTEA/ProyectoTEA/settings.py
+++ ProyectoTEA/ProyectoTEA/settings.py
@@ -85,9 +85,9 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
 USE_I18N = True
 
