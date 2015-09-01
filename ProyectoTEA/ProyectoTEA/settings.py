b5dcbf21dfc71e55fe88454e011c054840e79757
Luciano Castillo <luciano@localhost.localdomain>
2015-09-01 12:38:35 -0300

    Actualizacion de base de datos

--- ProyectoTEA/ProyectoTEA/settings.py
+++ ProyectoTEA/ProyectoTEA/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ProyectoTEA.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'proyectotea',
+        'USER': 'tea',
+        'PASSWORD': 't3a',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
