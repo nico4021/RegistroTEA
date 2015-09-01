51dcd62dc7c116d43ff8206e08522a567f6f2555
nico4021 <nico-bone@hotmail.com>
2015-09-01 11:51:54 -0300

    Models en proceso

--- ProyectoTEA/AppTEA/models.py
+++ ProyectoTEA/AppTEA/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
+
+#Le agrego area al profesional.
+class Profesional(User):
+    area = models.ManyToManyField(Area)
+
+class Area(models.Model):
+    nombre = models.CharField(max_length=40)
+    class Meta:
+        verbose_name = 'area'
+        verbose_name_plural = 'areas'
+
+# class Paciente(models.Model):
+#     dni
+#     apellidos
+#     diagnostico
+#     obra_social
