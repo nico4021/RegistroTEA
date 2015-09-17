b8055c2d87e445751f36607b395e591ebf2ecb6f
Luciano Castillo <luciano@localhost.localdomain>
2015-09-17 15:37:08 -0300

    Configurando base de datos

--- ProyectoTEA/ProyectoTEA/settings.py
+++ ProyectoTEA/ProyectoTEA/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'proyectotea',
-        'USER': 'root',
-        'PASSWORD': '',
+        'USER': 'tea',
+        'PASSWORD': 't3a',
         'HOST': 'localhost',
         'PORT': '3306',
     }
