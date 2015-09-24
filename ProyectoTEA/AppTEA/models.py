96d10467804a396f69b64a68a21c816621284ffc
LuchoCastillo <lucho.castillo97@gmail.com>
2015-09-24 14:42:30 -0300

    Combinando proyectos separados

--- ProyectoTEA/AppTEA/models.py
+++ ProyectoTEA/AppTEA/models.py
@@ -4,13 +4,6 @@ from django.contrib.auth.models import User
 from datetime import date
 
 
-#Le agrego area al profesional.
-#class Profesional(User):
-#    area = models.ManyToManyField(Area)
-#
-# Lo comente porque me tiraba error, Area no esta definido
-#
-
 class Area(models.Model):
     nombre = models.CharField(max_length=40)
     
