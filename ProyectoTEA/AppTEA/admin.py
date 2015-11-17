a4ea1177a210dad5670b693ae4ee63a412ae7dca
LuchoCastillo <lucho.castillo97@gmail.com>
2015-11-17 18:33:24 -0300

    Agregado Horario al admin

--- ProyectoTEA/AppTEA/admin.py
+++ ProyectoTEA/AppTEA/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Profesional, Presupuesto, Paciente, Area
+from .models import *
 
 # Register your models here.
 admin.site.register(Profesional)
 admin.site.register(Presupuesto)
 admin.site.register(Paciente)
 admin.site.register(Area)
+admin.site.register(Horario)
