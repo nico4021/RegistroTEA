ca89dd883c625cd4cba66414f79c841434f71b83
nachoborello <fanyt156974823@gmail.com>
2015-11-21 13:52:19 -0300

    calendario

--- ProyectoTEA/AppTEA/admin.py
+++ ProyectoTEA/AppTEA/admin.py
@@ -4,7 +4,7 @@ from .models import *
 
 
 # Register your models here.
-admin.site.register(Horario)
+
 admin.site.register(Profesional)
 admin.site.register(Presupuesto)
 admin.site.register(Paciente)
