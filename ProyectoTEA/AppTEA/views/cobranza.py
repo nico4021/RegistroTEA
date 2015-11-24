30a96e8abf973af82cbb1dec0c64db94593e6069
nachoborello <fanyt156974823@gmail.com>
2015-11-24 19:25:26 -0300

    cobranza listo

--- ProyectoTEA/AppTEA/views/cobranza.py
+++ ProyectoTEA/AppTEA/views/cobranza.py
@@ -16,13 +16,31 @@ Vista de cobranza.
 def mostrar(request):
     meses_aporte = []
     mes_year = datetime.now().strftime('%Y-%m')
-    objects = Mes_presupuesto.objects.filter()
+    if request.user.is_staff == True:
+        objects = Mes_presupuesto.objects.filter()
+    else:
+        objects = Mes_presupuesto.objects.filter(profesional = request.user.pk)
     for mes_aporte in objects:
         if mes_aporte.mes.strftime('%Y-%m') == mes_year:
             meses_aporte.append(mes_aporte)
             
     context = {
                "meses_aporte":meses_aporte
+               }
+                      
+    return render(request, "comun/cobranza/index.html", context)
+
+def mostrar_todas(request):
+    meses_aporte = []
+    mes_year = datetime.now().strftime('%Y-%m')
+    if request.user.is_staff == True:
+        objects = Mes_presupuesto.objects.filter()
+    else:
+        objects = Mes_presupuesto.objects.filter(profesional = request.user.pk)
+        
+    context = {
+               "titulo": "Cobranza General",
+               "meses_aporte":objects
                }
                       
     return render(request, "comun/cobranza/index.html", context)
